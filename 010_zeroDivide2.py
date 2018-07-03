80871e3b222f343972a1a9861925ac3aaa0d06da
DevilJinx <jxs66@163.com>
2018-07-03 20:44:38 +0800

    try和except
    如果在 try 子句中的代码导致一个错误，程序执行就立即转到except 子句的代码。在运行那些代码之后，执行照常继续。

---
+++ 010_zeroDivide2.py
@@ -0,0 +1,17 @@
+def spam(divideBy):
+    try:
+        return 42 / divideBy
+    except ZeroDivisionError:
+        print('Error: Invalid argument.')
+
+print(spam(2))
+print(spam(12))
+print(spam(0))
+print(spam(1))
+
+# Running result
+# 21.0
+# 3.5
+# Error: Invalid argument.
+# None
+# 42.0
