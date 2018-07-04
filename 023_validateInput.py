7170047fc09bf5ea001ef6f20af48791cf56f1b6
DevilJinx <jxs66@163.com>
2018-07-04 22:55:04 +0800

    isX 字符串方法

---
+++ 023_validateInput.py
@@ -0,0 +1,25 @@
+while True:
+    print('Enter your age: ')
+    age = input()
+    if age.isdecimal():
+        break
+    print('Please enter a number for your age.')
+
+while True:
+    print('Select a new password (letters and numbers only): ')
+    password = input()
+    if password.isalnum():
+        break
+    print('Passwords can only have letters and numbers. ')
+
+# Running result:
+# Enter your age:
+# twenty four
+# Please enter a number for your age.
+# Enter your age:
+# 24
+# Select a new password (letters and numbers only):
+# asd123!!!
+# Passwords can only have letters and numbers.
+# Select a new password (letters and numbers only):
+# asd123
