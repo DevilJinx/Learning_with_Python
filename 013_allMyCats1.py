8d476e8fb3313b508f7a5a7ffc7a7fc64277e90c
DevilJinx <jxs66@163.com>
2018-07-04 10:44:00 +0800

    使用列表
    不使用列表直接打印

---
+++ 013_allMyCats1.py
@@ -0,0 +1,31 @@
+print('Enter the name of cat 1: ')
+catName1 = input()
+print('Enter the name of cat 2: ')
+catName2 = input()
+print('Enter the name of cat 3: ')
+catName3 = input()
+print('Enter the name of cat 4: ')
+catName4 = input()
+print('Enter the name of cat 5: ')
+catName5 = input()
+print('Enter the name of cat 6: ')
+catName6 = input()
+print('The cat names are: ')
+print(catName1 + ' ' + catName2 + ' ' + catName3 + ' ' + 
+catName4 + ' ' + catName5 + ' ' + catName6)
+
+# Running result:
+# Enter the name of cat 1:
+# Devil
+# Enter the name of cat 2:
+# Jinx
+# Enter the name of cat 3:
+# is
+# Enter the name of cat 4:
+# a
+# Enter the name of cat 5:
+# handsome
+# Enter the name of cat 6:
+# boy
+# The cat names are:
+# Devil Jinx is a handsome boy
